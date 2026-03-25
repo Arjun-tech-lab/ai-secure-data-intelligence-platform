b49ca3670c21b7b2111e46224924a1c7f4ed87db
Arjun Indavara <arjunindavara@Arjuns-MacBook-Air.local>
2026-03-25 20:31:28 +0530

    frontend env setup for production

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,18 +1,16 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const analyzeFile = async (file: File | Blob) => {
   const formData = new FormData();
   formData.append("file", file);
 
   const response = await fetch(
-    "http://localhost:5000/api/analyze-file",
+    `${API_URL}/api/analyze-file`,
     {
       method: "POST",
       body: formData,
     }
   );
-
-  if (!response.ok) {
-    throw new Error("Failed to analyze file");
-  }
 
   return response.json();
 };
